40c500027ec54ff79d49cf8740837d7417e570dd
Pedro <pedbertasso@gmail.com>
2025-02-24 09:08:59 -0300

    Tratamento de erro

---
+++ ControleDeFluxo/src/Execoes.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Execoes {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) throws Exception {
+        try{
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+        
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();   
+        }
+        catch(java.util.InputMismatchException e){
+            System.out.println("Valor incorreto!!\nOs campos idade e altura precisam ser numéricos!!");
+        }
+    }
+}
